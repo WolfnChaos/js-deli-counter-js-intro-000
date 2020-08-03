6350dcae1d5107f0e6a57a016d4003e344275dec
Bradly Horn <wolfnchaos@gmail.com>
2020-08-03 18:16:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 function takeANumber(line, name) {
-  line.push(name);
-
-  let i = 0
-
-  while (i < line.lenght) {
-    return `Welcome, ${name[i]}. You are number ${line.lenght} in line.`
-    i++;
-  }
+[...line, name];
 }
