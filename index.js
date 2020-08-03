7fd1ef9a4bdca74f45c0436359a2b9a409dadddd
Bradly Horn <wolfnchaos@gmail.com>
2020-08-03 17:51:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-var katzDeliLine = [];
-
 function takeANumber(num, name) {
   let i = 0;
 
-  while (i < katzDeliLine.lenght) {
+  while (i < name.lenght) {
     return `Welcome, ${name[i]}. You are number ${num[i]} in line.`;
     i++;
   }
